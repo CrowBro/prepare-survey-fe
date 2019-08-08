47d747f7a3367e7c72725a71f0ccac3485101897
Benas Jacikas <benas@jacikas.lt>
2019-08-08 20:32:50 +0300

    Adding css baseline

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import SportsForm from "components/sportsForm";
 import SportsList from "components/sportsList";
 import Header from "components/headers";
@@ -9,14 +10,16 @@ import store from "store/store";
 
 export default () => (
     <Provider store={store}>
-        <BrowserRouter>
-            <Header />
-            <Switch>
-                <Route path="/sports/:id" component={SportsForm}/>
-                <Route path="/sports" component={SportsList}/>
-                <Route path="/survey" component={SurveyForm}/>
-                <Route path="/" component={SportsList}/>
-            </Switch>
-        </BrowserRouter>
+        <CssBaseline>
+            <BrowserRouter>
+                <Header />
+                <Switch>
+                    <Route path="/sports/:id" component={SportsForm}/>
+                    <Route path="/sports" component={SportsList}/>
+                    <Route path="/survey" component={SurveyForm}/>
+                    <Route path="/" component={SportsList}/>
+                </Switch>
+            </BrowserRouter>
+        </CssBaseline>
     </Provider>
 );
