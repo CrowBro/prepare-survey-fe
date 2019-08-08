ecf2d8221464f8330c82c8630de3d02df659aa1e
Benas Jacikas <benas@jacikas.lt>
2019-08-08 20:39:36 +0300

    Cleaning up table component

--- src/components/sportsList.tsx
+++ src/components/sportsList.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { useEffect, useState } from "react";
-import { makeStyles } from "@material-ui/styles";
 import Paper from "@material-ui/core/Paper";
 import Table from "@material-ui/core/Table";
 import TableCell from "@material-ui/core/TableCell";
@@ -9,17 +8,6 @@ import TableHead from "@material-ui/core/TableHead";
 import TableBody from "@material-ui/core/TableBody";
 import { RouteComponentProps, withRouter } from "react-router-dom";
 import { getSports, Sport } from "dataAccess/api";
-
-const useStyles = makeStyles({
-    appBar: {
-        width: "100%",
-        display: "flex",
-        zIndex: 1100,
-        boxSizing: "border-box",
-        flexShrink: 0,
-        flexDirection: "column",
-    }
-})
 
 const SportsList = (props: RouteComponentProps) => {
     const [ sports, setSports ] = useState<Sport[]>([])
@@ -30,32 +18,32 @@ const SportsList = (props: RouteComponentProps) => {
 
     return (
         <Paper>
-            <Table size="small">
+            <Table size="medium">
                 <TableHead>
                     <TableRow>
                         <TableCell>Sport ID</TableCell>
                         <TableCell>Name</TableCell>
-                        <TableCell align="right">Product 1</TableCell>
-                        <TableCell align="right">Product 2</TableCell>
-                        <TableCell align="right">Product 3</TableCell>
-                        <TableCell align="right">Product 4</TableCell>
-                        <TableCell align="right">Product 5</TableCell>
-                        <TableCell align="right">Product 6</TableCell>
-                        <TableCell align="right">Status</TableCell>
+                        <TableCell>Product 1</TableCell>
+                        <TableCell>Product 2</TableCell>
+                        <TableCell>Product 3</TableCell>
+                        <TableCell>Product 4</TableCell>
+                        <TableCell>Product 5</TableCell>
+                        <TableCell>Product 6</TableCell>
+                        <TableCell>Status</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {sports.map(sport => (
                         <TableRow key={sport.sportId} onClick={event => props.history.push(`/sports/${sport.sportId}`)}>
-                            <TableCell align="left">{sport.sportDisplayId}</TableCell>
-                            <TableCell align="left">{sport.sportName}</TableCell>
-                            <TableCell align="right">{sport.products[0]}</TableCell>
-                            <TableCell align="right">{sport.products[1]}</TableCell>
-                            <TableCell align="right">{sport.products[2]}</TableCell>
-                            <TableCell align="right">{sport.products[3]}</TableCell>
-                            <TableCell align="right">{sport.products[4]}</TableCell>
-                            <TableCell align="right">{sport.products[5]}</TableCell>
-                            <TableCell align="right">{sport.status}</TableCell>
+                            <TableCell>{sport.sportDisplayId}</TableCell>
+                            <TableCell>{sport.sportName}</TableCell>
+                            <TableCell>{sport.products[0]}</TableCell>
+                            <TableCell>{sport.products[1]}</TableCell>
+                            <TableCell>{sport.products[2]}</TableCell>
+                            <TableCell>{sport.products[3]}</TableCell>
+                            <TableCell>{sport.products[4]}</TableCell>
+                            <TableCell>{sport.products[5]}</TableCell>
+                            <TableCell>{sport.status}</TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
