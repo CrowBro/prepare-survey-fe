400b6aeb013b7009287f91f6d8c54718303405ea
Karolis Blazevicius <broliukas@gmail.com>
2019-08-12 02:46:43 +0300

    Fix the config file again...

--- src/dataAccess/apiConfig.ts
+++ src/dataAccess/apiConfig.ts
@@ -1,6 +1,6 @@
 export const apiConfig = {
-    baseUrl: "http://localhost:4000",
-    //baseUrl: "http://217.182.158.166:4000",
+    //baseUrl: "http://localhost:4000",
+    baseUrl: "http://217.182.158.166:4000",
     //countrySpace: "dev",
     countrySpace: "fr",
 }
