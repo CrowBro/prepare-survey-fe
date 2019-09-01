0f820f0ce72a2605dfc875faa4380a2ec2e33c58
crowbro <karolis@foje.lt>
2019-09-02 00:49:38 +0300

    Change config. Point to domain BE.

--- src/dataAccess/apiConfig.ts
+++ src/dataAccess/apiConfig.ts
@@ -1,7 +1,8 @@
 export const apiConfig = {
     // baseUrl: "http://localhost:4000", // when dev and no docker
     // baseUrl: "http://localhost:80", // when dev with docker
-    baseUrl: "http://217.182.158.166:80", // when staging with docker
+    baseUrl: "https://produits-gagnants.decathlon.fr:80", // when staging with docker and domain
+    //baseUrl: "http://217.182.158.166:80", // when staging with docker
     //baseUrl: "http://217.182.158.166:4000", //when staging and no docker
     //countrySpace: "dev",
     defaultCountrySpace: "fr",
