696563039e1c372654fd8ffdf97b34636578fdfc
crowbro <karolis@foje.lt>
2019-09-02 00:12:52 +0300

    Fixup.

--- src/dataAccess/apiConfig.ts
+++ src/dataAccess/apiConfig.ts
@@ -1,7 +1,7 @@
 export const apiConfig = {
     // baseUrl: "http://localhost:4000", // when dev and no docker
     // baseUrl: "http://localhost:80", // when dev with docker
-     baseUrl: "http://217.182.158.166:80", // when staging with docker
+    baseUrl: "http://217.182.158.166:80", // when staging with docker
     //baseUrl: "http://217.182.158.166:4000", //when staging and no docker
     //countrySpace: "dev",
     defaultCountrySpace: "fr",
