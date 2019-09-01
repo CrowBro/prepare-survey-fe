d9f67ab38e68470183965496a72fd9fa1074ce35
crowbro <karolis@foje.lt>
2019-09-02 01:08:05 +0300

    Fix config.

--- src/dataAccess/apiConfig.ts
+++ src/dataAccess/apiConfig.ts
@@ -1,7 +1,7 @@
 export const apiConfig = {
     // baseUrl: "http://localhost:4000", // when dev and no docker
     // baseUrl: "http://localhost:80", // when dev with docker
-    baseUrl: "https://produits-gagnants.decathlon.fr", // when staging with docker and domain
+    baseUrl: "https://produits-gagnants.decathlon.fr/preparation", // when staging with docker and domain
     //baseUrl: "http://217.182.158.166:80", // when staging with docker
     //baseUrl: "http://217.182.158.166:4000", //when staging and no docker
     //countrySpace: "dev",
