cc91d69df4a012a003f3daca7cafa7dc18e1669e
crowbro <karolis@foje.lt>
2019-09-04 22:27:11 +0300

    Remove some comments.

--- src/components/callbackComponent.tsx
+++ src/components/callbackComponent.tsx
@@ -8,12 +8,8 @@ import * as QueryString from "query-string";
 const CallbackComponent = (props: RouteComponentProps) => {
 
     let hash = props.location.hash;
-
-    console.log("CCCCCC:", hash)
     const parsed = QueryString.parse(hash);
-    console.log("DDDDD:", parsed)
     const authHeader = `${parsed.token_type} ${parsed.access_token}`;
-
     const moveToSports = (state: { countrySpace: string; authHeader: string }) => props.history.push({
         pathname: "/sports",
         state: state
