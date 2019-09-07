0d1337d128786012378cef2e19cb494f364b8aa3
crowbro <karolis@foje.lt>
2019-09-07 17:38:56 +0300

    Change prod callback url.

--- src/dataAccess/apiConfig.ts
+++ src/dataAccess/apiConfig.ts
@@ -2,6 +2,6 @@ export const apiConfig = {
     baseUrl: "https://produits-gagnants.decathlon.fr/preparation", // when staging with docker and domain
     defaultCountrySpace: "fr", 
     authClientId: "C344d538f23aa65cec06705296f9034ce5d5e657c",
-    authCallbackUrl: "http://217.182.158.166:80/api/login/callback",
+    authCallbackUrl: "https://produits-gagnants.decathlon.fr/preparation/api/login/callback",
     authAuthUrl: "https://preprod.idpdecathlon.oxylane.com/as/authorization.oauth2",
 }
