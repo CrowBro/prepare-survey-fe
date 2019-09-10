e537cf1eda026619bfad1566672608feae1dd10e
crowbro <karolis@foje.lt>
2019-09-10 21:13:12 +0300

    fixup client id.

--- src/dataAccess/apiConfig.ts
+++ src/dataAccess/apiConfig.ts
@@ -1,7 +1,7 @@
 export const apiConfig = {
     baseUrl: "https://produits-gagnants.decathlon.fr/preparation", // when staging with docker and domain
     defaultCountrySpace: "fr", 
-    authClientId: "C02192c9da610262216c2b812a5595ea5e8188505",
+    authClientId: "C02192c9da610262216c2b812a5995ea5e8188505",
     authCallbackUrl: "https://produits-gagnants.decathlon.fr/preparation/api/login/callback",
     authAuthUrl: "https://idpdecathlon.oxylane.com/as/authorization.oauth2",
 }
