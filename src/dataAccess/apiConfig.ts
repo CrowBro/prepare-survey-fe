df10f914662d6424f0325e11e71d294a92718531
crowbro <karolis@foje.lt>
2019-09-11 23:16:30 +0300

    fix preprod config.

--- src/dataAccess/apiConfig.ts
+++ src/dataAccess/apiConfig.ts
@@ -1,7 +1,7 @@
 export const apiConfig = {
-    baseUrl: "http://217.182.158.166:4000", //when staging and no docker
+    baseUrl: "https://produits-gagnants.decathlon.fr/preparation2", //when staging and no docker
     defaultCountrySpace: "fr",
     authClientId: "C344d538f23aa65cec06705296f9034ce5d5e657c",
-    authCallbackUrl: "https://produits-gagnants.decathlon.fr/preparation/api/login/callback",
+    authCallbackUrl: "https://produits-gagnants.decathlon.fr/preparation2/api/login/callback",
     authAuthUrl: "https://preprod.idpdecathlon.oxylane.com/as/authorization.oauth2",
 }
