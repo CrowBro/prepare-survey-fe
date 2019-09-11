9792cace66468b42d6fc47e8060e2b4bab80ffd0
crowbro <karolis@foje.lt>
2019-09-11 23:30:58 +0300

    push docker to a different repo for preprod

--- build-push-docker.sh
+++ build-push-docker.sh
@@ -1,5 +1,5 @@
 sudo ls &&
 yarn build &&
-sudo docker build -t prepare-survey-fe . &&
-sudo docker tag prepare-survey-fe crowbro/prepare-survey-fe &&
-sudo docker push crowbro/prepare-survey-fe
+sudo docker build -t prepare-survey-fe2 . &&
+sudo docker tag prepare-survey-fe2 crowbro/prepare-survey-fe2 &&
+sudo docker push crowbro/prepare-survey-fe2
