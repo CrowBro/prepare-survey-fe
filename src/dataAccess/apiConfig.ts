80d0b2bcb6dcf11a872dbd582e61356e1694520a
Povilas Simanskas <ps@frontit.dk>
2020-09-06 15:29:17 +0300

    local no docker(except db) setup

--- src/dataAccess/apiConfig.ts
+++ src/dataAccess/apiConfig.ts
@@ -1,5 +1,6 @@
 export const apiConfig = {
-    baseUrl: "http://localhost:3001",
+    //baseUrl: "http://localhost:3001",
+    baseUrl: "http://localhost:4000", //local no docker
     defaultCountrySpace: "fr",
     authClientId: "C344d538f23aa65cec06705296f9034ce5d5e657c",
     authCallbackUrl: "https://produits-gagnants.decathlon.fr/preparation2/api/login/callback",
