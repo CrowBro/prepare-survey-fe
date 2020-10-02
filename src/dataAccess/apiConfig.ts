339d79ae9c88d7c8dc3924696ad3764c1c7d36a0
Povilas Simanskas <ps@frontit.dk>
2020-10-02 20:00:32 +0300

    base url change

--- src/dataAccess/apiConfig.ts
+++ src/dataAccess/apiConfig.ts
@@ -1,5 +1,5 @@
 export const apiConfig = {
-    baseUrl: "https://produits-gagnants.decathlon.fr/preparation2", //when staging and no docker
+    baseUrl: "http://217.182.158.166:3000/",//"https://produits-gagnants.decathlon.fr/preparation2", //when staging and no docker
     defaultCountrySpace: "fr",
     authClientId: "C344d538f23aa65cec06705296f9034ce5d5e657c",
     authCallbackUrl: "https://produits-gagnants.decathlon.fr/preparation2/api/login/callback",
