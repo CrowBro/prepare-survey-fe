27933c96f0e07dc2a3a25879e1ff8c8f3acffec0
Povilas Simanskas <ps@frontit.dk>
2020-11-13 12:46:09 +0200

    i am oficially retarded

--- src/dataAccess/apiConfig.ts
+++ src/dataAccess/apiConfig.ts
@@ -2,6 +2,6 @@ export const apiConfig = {
     baseUrl: "https://produits-gagnants.decathlon.fr/preparation", //local no docker,   //"https://produits-gagnants.decathlon.fr/preparation2", //when staging and no docker
     defaultCountrySpace: "fr",
     authClientId: "C02192c9da610262216c2b812a5995ea5e8188505",//"C344d538f23aa65cec06705296f9034ce5d5e657c",
-    authCallbackUrl: "https://produits-gagnants.decathlon.fr/preparation2/api/login/callback",//"https://produits-gagnants.decathlon.fr/preparation2/api/login/callback",
+    authCallbackUrl: "https://produits-gagnants.decathlon.fr/preparation/api/login/callback",//"https://produits-gagnants.decathlon.fr/preparation2/api/login/callback",
     authAuthUrl: "https://idpdecathlon.oxylane.com/as/authorization.oauth2",//"https://preprod.idpdecathlon.oxylane.com/as/authorization.oauth2",
 }
