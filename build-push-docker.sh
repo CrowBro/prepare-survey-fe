b560bbca0513b7cf2660dcfe0da382c7da556b79
Povilas Simanskas <ps@frontit.dk>
2020-11-20 09:44:03 +0200

    hc

--- build-push-docker.sh
+++
@@ -1,5 +0,0 @@
-sudo ls &&
-yarn build &&
-sudo docker build -t prepare-survey-fe2 . &&
-sudo docker tag prepare-survey-fe2 crowbro/prepare-survey-fe2:staging &&
-sudo docker push crowbro/prepare-survey-fe2:staging
